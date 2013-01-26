546c942670de4be1a75014f40da8e222328d02a5
Aaron Cheng <acheng@college.harvard.edu>
2013-01-26 18:17:37 -0500

    played with index a bit

--- html/index.php
+++ html/index.php
@@ -11,9 +11,10 @@
     // configuration
     require("../includes/config.php"); 
 
-
+    $user_info = query("SELECT * FROM user WHERE u_id = ?", $_SESSION["u_id"]);
+    $full_name = $user_info[0]["full_name"];
  
     // render dashboard (the home page form), passing in information about EVERYTHING.
-    render("dashboard_form.php", array("title" => "Dashboard"));
+    render("dashboard_form.php", array("title" => "Dashboard", "full_name" = $full_name));
 
 ?>
