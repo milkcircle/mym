3b6438c5fe296867a52a0031aba83e8cec03482b
Aaron Cheng <acheng@college.harvard.edu>
2013-01-27 00:42:50 -0800

    "deleting" (hiding!) a med

---
+++ html/delete_med.php
@@ -0,0 +1,18 @@
+<?php
+
+  /***********************************************************************
+     * edit_med_details.php
+     *
+     * MYM
+     *
+     * Either updates medication details, or renders the form.
+     * $_SESSION["a_id"] MUST CONTAIN A VALUE.
+     * 
+     * TODO: MAKE SURE END THERAPY DATE IS AFTER BEGINNING THERAPY DATE!!!
+     * 
+     **********************************************************************/
+
+    // configuration
+    require("../includes/config.php");
+
+?>
