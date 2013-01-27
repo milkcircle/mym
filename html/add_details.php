39b6ee6cfdc06e9e5db4044ee3799d4ad9e1b423
Aaron Cheng <acheng@college.harvard.edu>
2013-01-26 21:55:41 -0500

    Update html/add_details.php

--- html/add_details.php
+++ html/add_details.php
@@ -15,41 +15,21 @@
     // add to the user_medication table in the database
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        // simplify the variable name
-        $medication_name = $_POST["medication_name"];
-
-        /* check if the medication name is custom or not */
-
-        // retrieve m_id of entries that match medication_name from database
-        $results = query("SELECT m_id FROM medication WHERE CONCAT(proprietary_name, ' ', proprietary_name_suffix) = ?", $medication_name);
-
-        // if results is empty, add custom entry onto medication table
-        if (empty($results))
-        {
-            $check = query("INSERT INTO medication (proprietary_name, creator_id) VALUES (?, ?)", $medication_name, $_SESSION["u_id"]);
-            $results = query("SELECT m_id FROM medication WHERE proprietary_name = ?", $medication_name);
-        }
-
-        // set m_id as m_id of the first entry of results (multiple results may have been returned)
-        $m_id = $results[0]["m_id"];
+        // simplify variables, gather input
+        $refill_date = $_POST["refill_date"];
+        $start_date = $_POST["start_date"];
+        $end_date = $_POST["end_date"];
+        $details = $_POST["details"];
         
-        // now, insert this information into user_medication table
-            // represent today's date as SQL format
-            $today = date("Y-m-d");
-        
-        // insert information into association table
-        $check = query("INSERT INTO user_medication (u_id, m_id, start, b_hidden) VALUES (?, ?, ?, ?)", $_SESSION["u_id"], $m_id, $today, 0);
+        // insert into the user_medication table
+        $check = query("UPDATE user_medication SET refill = ?, start = ?, end = ?, details = ? WHERE
+            a_id = ?", $refill_date, $start_date, $end_date, $details);
         if ($check === false)
         {
-            echo "Something went wrong inserting into association array.";
+            echo "Update failed, for some reason.";
         }
         
-        // query the thing we JUST inserted, so that we can store that in SESSION
-        $a_id = mysql_insert_id();
-        $_SESSION["a_id"] = $a_id;
-        
-        // render the form for extra details
-        redirect("add_details.php");
+        redirect("index.php");
     }    
     else
     {
