8f0934eaae1e36fa74c83beae803973b60ed914e
Aaron Cheng <acheng@college.harvard.edu>
2013-01-26 23:34:30 -0500

    Update html/medication_list.php

--- html/medication_list.php
+++ html/medication_list.php
@@ -1,3 +1,21 @@
 <?php
 
+    /***********************************************************************
+     * medication_list.php
+     *
+     * MYM
+     *
+     * Renders the medication list.
+     **********************************************************************/
+
+    // configuration
+    require("../includes/config.php"); 
+
+    $user_info = query("SELECT * FROM user WHERE u_id = ?", $_SESSION["u_id"]);
+    $full_name = $user_info[0]["full_name"];
+ 
+    // render dashboard (the home page form), passing in information about EVERYTHING.
+    render("dashboard_form.php", array("title" => "Dashboard", "full_name" => $full_name));
+
+
 ?>
