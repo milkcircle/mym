41e0e2b2c65ed4604bc6d697727138b7e2575732
Aaron Cheng <acheng@college.harvard.edu>
2013-01-27 00:32:40 -0500

    Update templates/medication_list_form.php

--- templates/medication_list_form.php
+++ templates/medication_list_form.php
@@ -1,7 +1,25 @@
 Medication list
 
+<table>
 <?php
 
-  foreach($
+  // make a nice two-column row for each medication entry
+  foreach($rows as $row)
+  {
+    echo "<tr>";
+      echo "<td>";
+        echo "<div>"
+        
+    
+      echo "</td>";
+    
+      echo "<td>";
+    
+    
+    
+      echo "</td>";
+    echo "</tr>";
+  }
 
 ?>
+</table>
