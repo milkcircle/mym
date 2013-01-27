746dc1e46c866b4aa76bded31f8b56243e681581
Aaron Cheng <acheng@college.harvard.edu>
2013-01-26 22:16:10 -0500

    todo comment

--- html/add_details.php
+++ html/add_details.php
@@ -7,6 +7,9 @@
      *
      * Either updates medication details, or renders the form.
      * $_SESSION["a_id"] MUST CONTAIN A VALUE.
+     * 
+     * TODO: MAKE SURE END THERAPY DATE IS AFTER BEGINNING THERAPY DATE!!!
+     * 
      **********************************************************************/
 
     // configuration
