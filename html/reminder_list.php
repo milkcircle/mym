25ca6f72b1321aed386950c748a2177f05f12692
Aaron Cheng <acheng@college.harvard.edu>
2013-01-27 04:56:47 -0800

    Create reminder_list.php

---
+++ html/reminder_list.php
@@ -0,0 +1,23 @@
+<?php
+
+    /***********************************************************************
+     * reminder_list.php
+     *
+     * MYM
+     *
+     * Renders the reminder list.
+     **********************************************************************/
+
+    // configuration
+    require("../includes/config.php"); 
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+
+    }
+    
+    else
+    {
+    }
+
+?>
