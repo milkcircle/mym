fe6deec2905cf19c0862c6a1e64c0e2200a7be01
Aaron Cheng <acheng@college.harvard.edu>
2013-01-27 00:00:35 -0500

    Update html/medication_list.php

--- html/medication_list.php
+++ html/medication_list.php
@@ -12,6 +12,8 @@
     require("../includes/config.php"); 
 
     $user_meds = query("SELECT * FROM user_medication WHERE u_id = ?", $_SESSION["u_id"]);
+    
+    
  
     // render dashboard (the home page form), passing in information about EVERYTHING.
     render("dashboard_form.php", array("title" => "Dashboard", "full_name" => $full_name));
