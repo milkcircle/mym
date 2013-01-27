7344b39c75b48271fc383b850bcfaf2eaea133f0
Aaron Cheng <acheng@college.harvard.edu>
2013-01-27 00:26:57 -0500

    Update html/medication_list.php

--- html/medication_list.php
+++ html/medication_list.php
@@ -11,23 +11,11 @@
     // configuration
     require("../includes/config.php"); 
 
-    // retrieves a 2d array of a_id values
-    $rows = query("SELECT a_id FROM user_medication WHERE u_id = ?", $_SESSION["u_id"]);
-    
-    // makes $rows into a single-dimension array called $a_id
-    $a_id = array();
-    $counter = 0;
-    foreach($rows as $row)
-    {
-        $a_id[$counter] = $row["a_id"];
-        $counter++;
-    }
- 
-    dump($a_id);
+    // retrieves a 2d array of information from the associative table
+    $rows = query("SELECT * FROM user_medication WHERE u_id = ?", $_SESSION["u_id"]);
     
     // render dashboard (the home page form), passing in information about EVERYTHING.
-    render("medication_list_form.php", array("title" => "Medication List", "user_meds" => $user_meds));
-
-
+    render("medication_list_form.php", array("title" => "Medication List", "rows" => $rows));
+    
 ?>
  
