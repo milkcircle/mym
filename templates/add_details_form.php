f7310768445fe304b2e4f840a27474889bb3e014
Aaron Cheng <acheng@college.harvard.edu>
2013-01-26 21:42:52 -0500

    worked on details_form template

--- templates/add_details_form.php
+++ templates/add_details_form.php
@@ -11,8 +11,29 @@
     <form action="add_details.php" method="post">
         <fieldset>
             <div class="control-group">
-                <p>Refill Date: <input type="text" id="datepicker" placeholder="<?= $refill?>"/></p>
+                <p>Refill Date: <input autofocus name="refill_date" type="text" id="datepicker" placeholder="<?= $refill?>"/></p>
             </div>
+            <br/><br/>
+            
+            <div class="control-group">
+                <p>Treatment Start Date: <input name="start_date" type="text" id="datepicker" placeholder="<?= $start?>"/></p>
+            </div>
+            <br/><br/>
+            
+            <div class="control-group">
+                <p>Treatment End Date: <input name="end_date" type="text" id="datepicker" placeholder="<?= $end?>"/></p>
+            </div>
+            <br/><br/>
+            
+            <div class="control-group">
+                <p>Drug details: <input name="details" type="text" placeholder="<?= $details?>"/></p>
+            </div>
+            <br/><br/>
+            
+            <div class="control-group">
+            <button type="submit" class="btn">Update Medication Details</button>
+            </div>
+            
         </fieldset>
     </form>
 </div>
