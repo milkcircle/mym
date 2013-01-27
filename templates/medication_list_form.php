f25d944a44b7eca160a97b26eb93060f559ed2ea
Aaron Cheng <acheng@college.harvard.edu>
2013-01-27 00:59:09 -0500

    VERY first try. Probably doesn't even work.

--- templates/medication_list_form.php
+++ templates/medication_list_form.php
@@ -7,15 +7,19 @@ Medication list
   foreach($rows as $row)
   {
     echo "<tr>";
+    
+      // print medication details
       echo "<td>";
         echo "<div><b>Name: </b>$row[\"proprietary_name\"] $row[\"proprietary_name_suffix\"]</div>";
-        echo "<div><b>Details: </b>$row[\"details\"];
+        echo "<div><b>Details: </b>$row[\"details\"]";
       echo "</td>";
   
+      // print buttons for different actions
       echo "<td>";
-        echo 
-    
-    
+        echo "<form action=\"medication_list.php\" method=\"post\">";
+          echo "<button type=\"submit\" class=\"btn\" value=\"$row[\"a_id\"]\">Edit Details</button>";
+          echo "";
+        echo "</form>";
       echo "</td>";
     echo "</tr>";
   }
