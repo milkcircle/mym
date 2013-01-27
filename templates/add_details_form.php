aaee221ed74c4ae689f30f1b991035d1a7edc4cd
Aaron Cheng <acheng@college.harvard.edu>
2013-01-26 20:56:05 -0500

    datepicker try1

--- templates/add_details_form.php
+++ templates/add_details_form.php
@@ -1,11 +1,14 @@
+<script>
+    $(function() {
+        $( "#datepicker" ).datepicker();
+    });
+</script>
+
 <div>
     <form action="add_details.php" method="post">
         <fieldset>
             <div class="control-group">
-                <input autofocus name="Refill Date" placeholder="Refill Date" type="text" id="refill_date"/>
-            </div>
-            <div class="control-group">
-                <button type="submit" class="btn">Update Refill Date</button>
+                <input autofocus type="text" class="datepicker" id="refill_date"/>
             </div>
         </fieldset>
     </form>
