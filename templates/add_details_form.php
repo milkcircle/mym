53cb82a5028daf8efba93ed5a76804973463b92e
Aaron Cheng <acheng@college.harvard.edu>
2013-01-26 17:03:46 -0800

    Create add_details_form.php

---
+++ templates/add_details_form.php
@@ -0,0 +1,12 @@
+<div>
+    <form action="add_details.php" method="post">
+        <fieldset>
+            <div class="control-group">
+                <input autofocus name="Refill Date" placeholder="Refill Date" type="text" id="refill_date"/>
+            </div>
+            <div class="control-group">
+                <button type="submit" class="btn">Update Refill Date</button>
+            </div>
+        </fieldset>
+    </form>
+</div>
